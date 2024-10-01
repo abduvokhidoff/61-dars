5c76609c3cb49d6c80da785575bce5d1140b130d
abduvokhidoff <abcdefgmananam@gmail.com>
2024-10-01 21:40:17 +0500

    done

---
+++ main.js
@@ -0,0 +1,66 @@
+// let wrapper = document.querySelector('.wrapper')
+// wrapper.style.display = 'flex'
+// wrapper.style.gap = '50px'
+
+// let wrapper_2 = document.querySelector('.wrapper_2')
+// let input = document.querySelector('input')
+// let button = document.querySelector('button')
+// let img = document.querySelector('img')
+
+
+
+// // const AddImageFunction = () => {
+// //     img.src = input.value
+// //     img.setAttribute("url", "img")
+// //     img.style.width= '500px'
+// //     img.style.heihgt = '500px'
+// // }
+// // button.addEventListener('click', () => {
+// //     AddImageFunction()
+// // })
+
+// button.addEventListener('click', ()=>{
+//     img.setAttribute('src', input.value)
+// })
+
+// let wrapper = document.querySelector('.wrapper')
+// wrapper.style.display = 'flex'
+// wrapper.style.gap = '50px'
+
+// let button = document.querySelectorAll('button')
+// let img = document.querySelector('img')
+
+// button[0].addEvenListener('click', () => {
+// img.setAttribute('src', 'https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg')
+// })
+// button[1].addEvenListener('click', () =>{
+//     img.setAttribute('src', 'https://img.freepik.com/free-photo/blue-house-with-blue-roof-sky-background_1340-25953.jpg')
+// })
+// button[2].addEvenListener('click', () => {
+//     img.setAttribute('src', 'https://cdn.pixabay.com/photo/2023/02/07/17/49/supercar-7774683_640.jpg')
+// })
+
+
+let body = document.querySelector('body')
+let p = document.querySelector('p')
+
+body.style.display = 'none'
+p.style.display = 'none'
+
+let body_color = prompt('Enter body background-color!')
+let p_color = prompt('Enter text color!')
+
+body.style.backgroundColor = body_color
+p.style.color = p_color
+
+
+
+
+
+
+
+body.style.display = 'block'
+
+p.style.display = 'block'
+
+
